7b37da72bee2528cbf86c08bc3c787462657f6fd
Aleksandr Podlesnov <sawawawre@gmail.com>
2024-01-24 21:53:44 +0300

    'добавил тесты к новым функциям'

--- tests/test_item.py
+++ tests/test_item.py
@@ -17,3 +17,23 @@ def test_calculate_total_price():
     item3.apply_discount()
     assert item3.price == 8000.0
     assert item4.price == 20000
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv("items.csv")
+    assert len(Item.all) == 5
+
+    item = Item.all[0]
+    assert item.name == 'Смартфон'
+
+    item.name = 'Телефон'
+    assert item.name == 'Телефон'
+
+    item.name = 'СуперСмартфон'
+    assert item.name == 'СуперСмарт'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('10') == 10
+    assert Item.string_to_number('10.0') == 10
+    assert Item.string_to_number(10.10) == 10
