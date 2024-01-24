47f28c3552b6708e929ebc46c5d9ae0952b7318a
Aleksandr Podlesnov <sawawawre@gmail.com>
2024-01-24 21:53:26 +0300

    'добавил декоратор property, реализовал classmethod и staticmethod'

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+from pathlib import Path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -32,3 +36,27 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self.__name = value
+        else:
+            self.__name = value[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, csvfile):
+        cls.all.clear()
+        csvfile = Path(__file__).parent.joinpath("items.csv")
+        with open(csvfile, 'r', newline='', encoding="windows-1251") as file:
+            reader = csv.DictReader(file)
+            for emp in reader:
+                cls(emp['name'], float(emp['price']), int(emp['quantity']))
+
+    @staticmethod
+    def string_to_number(valuestr):
+        return int(float(valuestr))
