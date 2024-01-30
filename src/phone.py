a7f8e6ddeab5f86c339ae4ced8fe8211001eef82
Aleksandr Podlesnov <sawawawre@gmail.com>
2024-01-30 22:50:05 +0300

    'Создал новый класс, с атрибутами родительского класса, реализовал методы согласно заданию'

---
+++ src/phone.py
@@ -0,0 +1,37 @@
+from src.item import Item
+
+
+class Phone(Item):
+
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if int(value) >= 0:
+            self.__number_of_sim = int(value)
+        else:
+            print('ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.')
+
+    def __repr__(self):
+        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            print('Класс не является дочерним классом Item')
+
+        return self.quantity + other.quantity
+
+    def __radd__(self, other):
+        if not isinstance(other, Item):
+            print('Класс не является дочерним классом Item')
+
+        return other.quantity + self.quantity
